c4485f0f307603b1e93c33ac92ab475bc9781cf5
quetourah <dina.qd.academics@gmail.com>
2018-10-28 04:26:18 -0400

    added user object

--- app.js
+++ app.js
@@ -19,5 +19,53 @@ app.get('/login', (req, res) =>
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 
+var Sequelize = require('sequelize');
+
+var connection = new Sequelize('postgres', 'postgres','password',{ dialect: 'postgres'});
+
+var User = connection.define('user',{
+	firstname: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	lastname: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	email: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		primaryKey: true,
+		unique: true,
+		validate: {
+			isEmail: true
+		}
+	},
+	courses: {
+		type: Sequelize.STRING,
+	},
+	bio: {
+		type: Sequelize.TEXT
+	},
+	langproficiency: {
+		type: Sequelize.STRING,
+	},
+	ismentor:{
+		type: Sequelize.BOOLEAN,
+		defaultValue: false
+	}
+
+});
+
+connection.sync();
+//.then(function (){
+// 	User.create({
+// 		username: 'sangria',
+// 		password: 'wine',
+// 		email: 'email@email.com',
+// 		interest: 'computer science'
+// 	});
+	
+// });
 
 
